0924ad19f8eff201ec25fec7f472fd31d2315425
NadyaB90 <lady.nadya20@mail.ru>
2022-06-07 13:22:12 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "5539657750:AAG5vpFbvsozlRHOv_4sj4XOk80krDsm4Ck"
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ main.py
@@ -0,0 +1,36 @@
+import telebot
+from extensions import APIException, Converter
+from config import *
+import traceback
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def hello(message: telebot.types.Message):
+    text = 'Чтобы узнать курс валют, введите команду боту в следующем порядке: <имя валюты> <в какую валюту перевести> <количество переводимой валюты>'
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values_1 = message.text.split()
+    try:
+        if len(values_1) != 3:
+            raise APIException('Неверное количество параметров!')
+        
+        answer = Converter.get_price(*values_1)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка в команде:\n{e}" )
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}" )
+    else:
+        bot.reply_to(message,answer)
+
+bot.polling()
